c8e5bc7fa86fde331cd17d8181a771070d385d89
hafizh <hafizhyastito@gmail.com>
2026-04-02 11:49:59 +0700

    practice

--- menghitungIP.java
+++ menghitungIP.java
@@ -11,7 +11,7 @@ public class menghitungIP {
     ips = scanner.nextDouble();
     
     //hitung sks
-    if (ips>=3.50) {
+    if (ips >= 3.50 && ips <= 4.00) {
         sks = 24;
     } else if (ips >= 3.00 && ips <= 3.49) {
         sks = 22;
@@ -26,5 +26,30 @@ public class menghitungIP {
     }
     System.out.println("Jumlah SKS yang dapat diambil: " + sks);
     scanner.close();
+
+    //status sks
+    switch (sks) {
+        case 24:
+            System.out.println("Status: Cum Laude");
+            break;
+    case 22:
+            System.out.println("Status: Sangat Memuaskan");
+            break;
+    case 20:
+            System.out.println("Status: Memuaskan");
+            break;
+    case 18:
+            System.out.println("Status: Cukup");
+            break;
+    case 0:
+            System.out.println("Status: Tidak Lulus");
+            break;
+        default:
+            System.out.println("Status: Tidak Diketahui");
+            break;
+    }
+
+
+
 }
 }
